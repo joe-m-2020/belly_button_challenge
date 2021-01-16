12a821a55dbbf9815315b898d750529d9996b9f6
Mr Magpie <joe.marrone@outlook.com>
2021-01-15 22:48:44 -0600

    returned first result for sample values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,22 +1,22 @@
 // data path
 const path = "samples.json"
-// Fetch the JSON data and console log it
+// // Fetch the JSON data and console log it
 d3.json(path).then(function(data) {
-    console.log(data);
+    console.log(data.samples[0].sample_values);
   });
 //create function to unpack json data
-function unpack(rows, index) {
+function unpack(rows, index,value) {
     return rows.map(function(row) {
-      return row[index];
+      return row[0].value;
     });
   }
 // Fetch the JSON data and console log it
 function buildPlot() {
     d3.json(path).then(function(data){
         console.log(data);
-        console.log(data.samples)
+        // console.log(data.samples)
         //grab values for plotting
-        var sample_vals= unpack(data.samples,  0);
+        var sample_vals= unpack(data.samples, 0);
         console.log(sample_vals);
         var otu_ids = unpack(data.samples, 1);
         console.log(otu_ids);
