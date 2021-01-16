fc9acd608dd692c101a5d840a57bf348681c3916
Mr Magpie <joe.marrone@outlook.com>
2021-01-16 14:34:16 -0600

    loop for sample values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,25 +1,27 @@
 // data path
 const path = "samples.json"
 // // Fetch the JSON data and console log it
-d3.json(path).then(function(data) {
-    console.log(data.samples[0].sample_values);
-  });
-//create function to unpack json data
-function unpack(rows, index,value) {
-    return rows.map(function(row) {
-      return row[0].value;
-    });
-  }
+// d3.json(path).then(function(data) {
+//     // map(data.samples[0].sample_values);
+//     data.samples.forEach(element => {
+//         console.log(element.sample_values);
+        
+//     });
+//   });
+// //create function to unpack json data
+// function unpack(rows, index ){
+//     return rows.map(function(row) {
+//       return row[index]
+//     });
+//   }
 // Fetch the JSON data and console log it
 function buildPlot() {
-    d3.json(path).then(function(data){
-        console.log(data);
-        // console.log(data.samples)
-        //grab values for plotting
-        var sample_vals= unpack(data.samples, 0);
-        console.log(sample_vals);
-        var otu_ids = unpack(data.samples, 1);
-        console.log(otu_ids);
-    })
+    d3.json(path).then(function(data) {
+        // map(data.samples[0].sample_values);
+        data.samples.forEach(element => {
+            console.log(element.sample_values);
+            
+        });
+    });
 }
 buildPlot()
