51cf1bc5fb2827dea08872e81820e5b6b412b7a4
Mr Magpie <joe.marrone@outlook.com>
2021-01-16 23:50:08 -0600

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,56 +12,102 @@ d3.json(path).then(function(data){
         dropdownMenu.append("option").attr("value",element).text(element);
     
     });
+    d3.selectAll("selDataset").on("change",function(subject) {
+        buildBarPlot(subject)
+    }
 });
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-        return row[index];
-    });
-};
-
-function buildBarPlot() {
-        d3.json(path).then(function(data) {
-            console.log(data.samples)
-            var names = data.names;
-            var name = names.indexOf("940");
-            var sample_values = unpack(data.samples, 3);
-            console.log(sample_values);
-    });
-};    
+  
 // d3.selectAll("selDataset").on("change", buildBarplot(subject)
 
 // Fetch the JSON data and save as variables
-// function buildBarPlot() {
-//     d3.json(path).then(function(data) {
-//         var names = data.names
-//         var name = names.indexOf("940")
-//         console.log(name)
-//         // console.log(data.samples)
-//         // map(data.samples[0].sample_values);
-//         data.samples.forEach(element => {
-//             var dataset = []
-//             var sample_values_all = element.sample_values;
-//             var sample_values_desc = sample_values_all.slice(0, 10);
-//             var sample_values = sample_values_desc.reverse();
-//             dataset.append(sample_values)
-//             console.log(dataset[name])
-//         });
-//         data.samples.forEach(element => {
-//             var otu_ids_all = element.otu_ids;
-//             var otu_ids_desc = otu_ids_all.slice(0, 10);
-//             var otu_ids = otu_ids_desc.reverse();
-//             // console.log(otu_ids)
+function buildBarPlot() {
+    // data path
+const path = "samples.json"
+
+
+// get names for dropdown menu
+
+d3.json(path).then(function(data){
+    var names = data.names;
+    console.log(names);
+    var dropdownMenu = d3.select("#selDataset");
+    names.forEach(element=> {
+        dropdownMenu.append("option").attr("value",element).text(element);
+    
+    });
+d3.selectAll("selDataset").on("change",function(subject) {
+    buildBarPlot(subject)
+    }
+});
+
+  
+// d3.selectAll("selDataset").on("change", buildBarplot(subject)
+
+// Fetch the JSON data and save as variables
+function buildBarPlot() {
+    var subject = d3.select("#selDataset")
+    d3.json(path).then(function(data) {
+        var names = data.names
+        var name = names.indexOf(subject)
+        console.log(name)
+        // console.log(data.samples)
+        // map(data.samples[0].sample_values);
+        data.samples.forEach(element => {
+            var dataset = []
+            var sample_values_all = element.sample_values;
+            var sample_values_desc = sample_values_all.slice(0, 10);
+            var sample_values = sample_values_desc.reverse();
+            dataset.append(sample_values)
+            console.log(dataset[name])
+        });
+        data.samples.forEach(element => {
+            var otu_ids_all = element.otu_ids;
+            var otu_ids_desc = otu_ids_all.slice(0, 10);
+            var otu_ids = otu_ids_desc.reverse();
+            // console.log(otu_ids)
            
             
-//         });
-//         data.samples.forEach(element => {
-//             var otu_labels_all = element.otu_labels;
-//             var otu_labels_desc = otu_labels_all.slice(0, 10);
-//             var otu_labels = otu_labels_desc.reverse();
-//             // console.log(otu_labels)
-//         });
+        });
+        data.samples.forEach(element => {
+            var otu_labels_all = element.otu_labels;
+            var otu_labels_desc = otu_labels_all.slice(0, 10);
+            var otu_labels = otu_labels_desc.reverse();
+            // console.log(otu_labels)
+        });
 
-//     });
-// }
+    });
+}
+buildBarPlot()
+    d3.json(path).then(function(data) {
+        var names = data.names
+        var name = names.indexOf("940")
+        console.log(name)
+        // console.log(data.samples)
+        // map(data.samples[0].sample_values);
+        data.samples.forEach(element => {
+            var dataset = []
+            var sample_values_all = element.sample_values;
+            var sample_values_desc = sample_values_all.slice(0, 10);
+            var sample_values = sample_values_desc.reverse();
+            dataset.append(sample_values)
+            console.log(dataset[name])
+        });
+        data.samples.forEach(element => {
+            var otu_ids_all = element.otu_ids;
+            var otu_ids_desc = otu_ids_all.slice(0, 10);
+            var otu_ids = otu_ids_desc.reverse();
+            // console.log(otu_ids)
+           
+            
+        });
+        data.samples.forEach(element => {
+            var otu_labels_all = element.otu_labels;
+            var otu_labels_desc = otu_labels_all.slice(0, 10);
+            var otu_labels = otu_labels_desc.reverse();
+            // console.log(otu_labels)
+        });
+
+    });
+}
 buildBarPlot()
