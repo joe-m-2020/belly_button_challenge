daa2f3e20abf0f1189660a960f784446532316e5
Mr Magpie <joe.marrone@outlook.com>
2021-01-16 19:30:16 -0600

    updated get samples data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -15,13 +15,27 @@ const path = "samples.json"
 //     });
 //   }
 // Fetch the JSON data and console log it
-function buildPlot() {
+function getSamplesData() {
     d3.json(path).then(function(data) {
         // map(data.samples[0].sample_values);
         data.samples.forEach(element => {
-            console.log(element.sample_values);
-            
+            var sample_values_all = element.sample_values;
+            var sample_values_desc = sample_values_all.slice(0, 10);
+            var sample_values = sample_values_desc.reverse();
+            console.log(sample_values)
+        });
+        data.samples.forEach(element => {
+            var otu_ids_all = element.otu_ids;
+            var otu_ids_desc = otu_ids_all.slice(0, 10);
+            var otu_ids = otu_ids_desc.reverse();
+            console.log(otu_ids)
+        });
+        data.samples.forEach(element => {
+            var otu_labels_all = element.otu_labels;
+            var otu_labels_desc = otu_labels_all.slice(0, 10);
+            var otu_labels = otu_labels_desc.reverse();
+            console.log(otu_labels)
         });
     });
 }
-buildPlot()
+getSamplesData()
