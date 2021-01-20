f0081d6e33e65336d84eab54e640a45cc3b6ab52
Mr Magpie <joe.marrone@outlook.com>
2021-01-19 20:17:52 -0600

    update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,8 @@
 function init() {
     const selectedSubjectID = parseInt(d3.select('#selDataset').property('value'));
     buildBarPlot(selectedSubjectID);
+    popMetadata(selectedSubjectID);
+
 }
 
 // data path
@@ -17,12 +19,14 @@ d3.json(path).then(function(data){
     init();
 });
 
+
   
 // handle on click event
 d3.select('#selDataset')
   .on('change', function() {
     const selectedSubjectID = parseInt(d3.select(this).property('value'));
     buildBarPlot(selectedSubjectID);
+    popMetadata(selectedSubjectID);
 });
 
 // Fetch the JSON data and save as variables
@@ -32,6 +36,7 @@ function buildBarPlot(selectedSubjectID) {
 
         // subject data handling
         const selectedSubject = metadata.find(item => item.id === selectedSubjectID);
+        console.log(metadata);
         // end subject data handling
 
         // begin sample data handling
@@ -40,29 +45,65 @@ function buildBarPlot(selectedSubjectID) {
 
         const first10OTUIDsReversed = otu_ids.slice(0, 10).reverse();
         console.log(first10OTUIDsReversed);
-        const first10OUTOLabelssReversed = otu_labels.slice(0, 10).reverse();
-        console.log(first10OUTOLabelssReversed);
+        const first10OTULabelssReversed = otu_labels.slice(0, 10).reverse();
+        console.log(first10OTULabelssReversed);
         const first10OSamplesReversed = sample_values.slice(0, 10).reverse();
         console.log(first10OSamplesReversed);
         // end sample data handling
+        var strOTUids = first10OTUIDsReversed.map(function(e) {return e.toString()});
+        console.log(strOTUids)
+        const str = 'OTU ';
+        var joinedOTUids = [];
+        for (var j = 0; j < strOTUids.length; j++) {
+            console.log(strOTUids[j]);
+            const str2 = str.concat(strOTUids[j]);
+            joinedOTUids.push(str2);
+        };
+        console.log(joinedOTUids);
+        var trace1 = {
+            x: first10OSamplesReversed,
+            y: joinedOTUids,
+            type: 'bar',
+            orientation: 'h',
+            text: first10OTULabelssReversed,
+            marker: {
 
-        // data.samples.forEach(element => {
-        //     var sample_values_all = element.sample_values;
-        //     var sample_values_desc = sample_values_all.slice(0, 10);
-        //     var sample_values = sample_values_desc.reverse();
-        // });
+                width: 1
+            }
+        };
+        var data1 = [trace1];
 
-        // data.samples.forEach(element => {
-        //     var otu_ids_all = element.otu_ids;
-        //     var otu_ids_desc = otu_ids_all.slice(0, 10);
-        //     var otu_ids = otu_ids_desc.reverse();
-        //     // console.log(otu_ids)
-        // });
-        // data.samples.forEach(element => {
-        //     var otu_labels_all = element.otu_labels;
-        //     var otu_labels_desc = otu_labels_all.slice(0, 10);
-        //     var otu_labels = otu_labels_desc.reverse();
-        //     // console.log(otu_labels)
-        // });
+        Plotly.newPlot('bar', data1);
     });
-}
+};
+
+function popMetadata(selectedSubjectID) {
+    d3.json(path).then(function(data) {
+        const metadata = data;
+
+        // subject data handling
+        const selectedSubject = metadata.find(item => item.id === selectedSubjectID);
+        
+        var subj_id = selectedSubject.id;
+        console.log(subj_id)
+        var ethnicity = selectedSubject.ethnicity;
+        var gender = selectedSubject.gender;
+        var age = selectedSubject.age;
+        var location = selectedSubject.location;
+
+        // end subject data handling
+        var table = d3.select('panel-body');
+        table.html('');
+        var trow = table.append('p');
+
+        trow.text(subj_id);
+        trow.text(ethnicity);
+        trow.text(gender);
+        trow.text(age);
+        trow.text(location);
+
+
+    });
+};
+
+// id: 940, ethnicity: "Caucasian", gender: "F", age: 24, location: "Beaufort/NC
